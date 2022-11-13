f7649471f66577a67e692afc535ccea31f902ad0
David Marambio <david.marambios@gmail.com>
2022-11-13 02:37:55 -0300

    Functions are crated to validate permissions by role

--- server/common/middleware/common.permission.middleware.ts
+++ server/common/middleware/common.permission.middleware.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { ForbiddenError } from "@casl/ability";
+import { PERMISSION, MODEL_NAMES } from '../permissions/roles.permission'
+import { getRoleAbilityForUser } from "../permissions/utils.permission";
 import debug from "debug";
 import { PermissionFlag } from "./common.permissionflag.enum";
+import usersService from "../../users/services/users.service";
 
 const log: debug.IDebugger = debug("app:common-permission-middleware");
+ForbiddenError.setDefaultMessage(error => `You are not allowed to ${error.action} on ${error.subjectType}`);
 
 class CommonPermissionMiddleware {
+
   permissionFlagRequired(requiredPermissionFlag: PermissionFlag) {
     return (
       _req: express.Request,
@@ -12,11 +18,11 @@ class CommonPermissionMiddleware {
       next: express.NextFunction
     ) => {
       try {
-        const userPermissionFlags = parseInt(res.locals.jwt.permissionFlags);
-        if (userPermissionFlags & requiredPermissionFlag) {
+        const userPermissionFlags = res.locals.jwt.permissionFlags;
+        if (userPermissionFlags === requiredPermissionFlag) {
           return next();
         } else {
-          return res.status(403).send();
+          return res.status(403).send({ error: "you don't have permission for this action" });
         }
       } catch (error) {
         log(error);
@@ -24,12 +30,412 @@ class CommonPermissionMiddleware {
     };
   }
 
+  async roleCanCreateUser(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.CREATE, MODEL_NAMES.USERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanCreatePaper(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.CREATE, MODEL_NAMES.PAPERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanCreateJournal(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.CREATE, MODEL_NAMES.JOURNALS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanCreateQuartile(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.CREATE, MODEL_NAMES.QUARTILES);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanCreateYear(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.CREATE, MODEL_NAMES.YEARS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanReadUser(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.READ, MODEL_NAMES.USERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanReadPaper(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.READ, MODEL_NAMES.PAPERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanReadJournal(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.READ, MODEL_NAMES.JOURNALS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanReadQuartile(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.READ, MODEL_NAMES.QUARTILES);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanReadYear(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.READ, MODEL_NAMES.YEARS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanUpdateUser(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.UPDATE, MODEL_NAMES.USERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanUpdatePaper(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.UPDATE, MODEL_NAMES.PAPERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanUpdateJournal(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.UPDATE, MODEL_NAMES.JOURNALS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanUpdateQuartile(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.UPDATE, MODEL_NAMES.QUARTILES);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanUpdateYear(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.UPDATE, MODEL_NAMES.YEARS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanDeleteUser(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.DELETE, MODEL_NAMES.USERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanDeletePaper(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.DELETE, MODEL_NAMES.PAPERS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanDeleteJournal(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.DELETE, MODEL_NAMES.JOURNALS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanDeleteQuartile(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.DELETE, MODEL_NAMES.QUARTILES);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
+  async roleCanDeleteYear(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const user = await usersService.readById(res.locals.jwt.userId);
+      if (user) {
+        const ability = getRoleAbilityForUser(user);
+        ForbiddenError.from(ability)
+          .throwUnlessCan(PERMISSION.DELETE, MODEL_NAMES.YEARS);
+        return next()
+      } else {
+        return res.status(403).send();
+      }
+    } catch (error) {
+      console.log({ message: error })
+    }
+  }
+
   async onlySameUserOrAdminCanDoThisAction(
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
   ) {
-    const userPermissionFlags = parseInt(res.locals.jwt.permissionFlags);
+    const userPermissionFlags = res.locals.jwt.permissionFlags;
     if (
       req.params &&
       req.params.userId &&
@@ -37,7 +443,7 @@ class CommonPermissionMiddleware {
     ) {
       return next();
     } else {
-      if (userPermissionFlags & PermissionFlag.ADMIN_PERMISSION) {
+      if (userPermissionFlags === PermissionFlag.ADMIN) {
         return next();
       } else {
         res.status(403).send();
