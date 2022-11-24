9c9d15589b633787513ec3a1bf0235297803e5eb
David Marambio <david.marambios@gmail.com>
2022-11-23 23:40:57 -0300

    Functions are updated so that when logging in a session is created

--- server/auth/auth.routes.config.ts
+++ server/auth/auth.routes.config.ts
@@ -1,32 +1,29 @@
-import express from "express";
-import { body } from "express-validator";
-import { CommonRoutesConfig } from "../common/common.routes.config";
-import bodyValidationMiddleware from "../common/middleware/body.validation.middleware";
-import authController from "./controllers/auth.controller";
-import authMiddleware from "./middleware/auth.middleware";
-import jwtMiddleware from "./middleware/jwt.middleware";
+import express from 'express'
+import { body } from 'express-validator'
+import { CommonRoutesConfig } from '../common/common.routes.config'
+import bodyValidationMiddleware from '../common/middleware/body.validation.middleware'
+import sessionController from '../session/controllers/session.controller'
+import jwtMiddleware from './middleware/jwt.middleware'
+import { inputSessionSchema } from '../session/dto/input.session.dto'
 
 export class AuthRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
-    super(app, "AuthRoutes");
+    super(app, 'AuthRoutes')
   }
 
   configureRoutes(): express.Application {
-    this.app.post("/auth", [
-      body("email").isEmail(),
-      body("password").isString(),
-      bodyValidationMiddleware.verifiBodyFieldsErrors,
-      authMiddleware.verifyUserpassword,
-      authController.createJWT,
-    ]);
+    this.app.post('/auth', [
+      body('email').isEmail(),
+      body('password').isString(),
+      bodyValidationMiddleware.verifiBodyFieldsErrors(inputSessionSchema),
+      sessionController.createSessionHandler
+    ])
 
-    this.app.post("/auth/refresh-token", [(
+    this.app.post('/auth/refresh-token', [(
       jwtMiddleware.validJwtNeeded,
-      jwtMiddleware.verifyRefreshBodyField,
-      jwtMiddleware.validRefreshNeeded,
-      authController.createJWT
-    )]);
+      sessionController.refreshAccessTokenHandler
+    )])
 
-    return this.app;
+    return this.app
   }
 }
