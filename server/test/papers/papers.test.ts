1d6e8a23c0c9846aafed5f99149d24ab2e8cc08e
David Marambio <david.marambios@gmail.com>
2022-12-14 23:37:06 -0300

    Tests are implemented for queries to the clarivate api

--- server/test/papers/papers.test.ts
+++ server/test/papers/papers.test.ts
@@ -1,19 +1,8 @@
-import supertest from 'supertest'
 import { expect } from 'chai'
 import app from '../../app'
+import supertest from 'supertest'
+import { after } from 'mocha'
 
-let accessToken = ''
-let adminUserId = ''
-let firstPaperIdTest = ''
-let verificationCode = ''
-const newPaperBody = {
-    year: 2016,
-    codeWos: "asdasdasd",
-    codeDoi: "asdasdasdasd",
-    typePaper: "Artículo",
-    journalName: "Nombre",
-    title: "Título",
-}
 const userAdmin = {
     firstName: "David",
     lastName: "Marambio",
@@ -22,43 +11,192 @@ const userAdmin = {
     passwordConfirmation: 'Sup3rSecret!23',
     role: "admin"
 }
+let adminUserId: string
+let verificationCode: string
+let accessToken: string
+let activeYear: number
+let journals = Array()
+let wosId: string
+
+
+let firstPaperIdTest = ''
+const newPaperBody = {
+    "year": 2021,
+    "codeWos": "WOS:000568731400017",
+    "codeDoi": "10.1016\/j.jfoodeng.2020.110230",
+    "typePaper": "Artículo",
+    "journalName": "JOURNAL OF FOOD ENGINEERING",
+    "journalVolume": 290,
+    "title": "Manufacture of beta-chitin nano- and microparticles from jumbo squid pen (Dosidicus gigas) and evaluation of their effect on mechanical properties and water vapour permeability of polyvinyl alcohol\/chitosan films",
+}
+
 const importPapers = [
     {
-        'year': 2016,
-        'codeWos': "asdasdasd",
-        'codeDoi': "asdasdasdasd",
-        'typePaper': "Artículo",
-        'journalName': "Nombre",
-        'journalNumber': 11,
-        'journalVolume': 1,
-        'title': "Título",
-        'chapterPage': 2,
-        'numberOfPages': 22,
-        'initialPage': 10,
-        'endPage': 32
+        "year": 2021,
+        "codeWos": "WOS:000687176700005",
+        "codeDoi": "10.1021\/acschemneuro.0c00762",
+        "typePaper": "Artículo",
+        "journalName": "ACS CHEMICAL NEUROSCIENCE",
+        "journalNumber": 16,
+        "journalVolume": 12,
+        "title": "Sex-Dependent Changes of miRNA Levels in the Hippocampus of Adrenalectomized Rats Following Acute Corticosterone Administration",
+        'chapterPage': 10,
+        'numberOfPages': 20,
+        "initialPage": 2981,
+        "endPage": 3001
     },
     {
-        'year': 2016,
-        'codeWos': "rtyrtyrty",
-        'codeDoi': "344erter",
-        'typePaper': "Artículo",
-        'journalName': "Nombre2",
-        'journalNumber': 11,
-        'journalVolume': 1,
-        'title': "Título2",
-        'chapterPage': 2,
-        'numberOfPages': 22,
-        'initialPage': 10,
-        'endPage': 32
-    }
+        "year": 2021,
+        "codeWos": "WOS:000613637800049",
+        "codeDoi": "10.1039\/d0nj04680a",
+        "typePaper": "Artículo",
+        "journalName": "NEW JOURNAL OF CHEMISTRY",
+        "journalNumber": 4,
+        "journalVolume": 45,
+        "title": "Influence of the Ni-II\/Mn-II ratio on the physical properties of heterometallic Ni2xMn(2-2x)P2S6 phases and potassium intercalates K0.8Ni2xMn(1.6-2x)P2S6 center dot 2H(2)O",
+        "initialPage": 2175,
+        "endPage": 2183
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000626476700034",
+        "codeDoi": "10.1016\/j.jconrel.2020.11.019",
+        "typePaper": "Artículo",
+        "journalName": "JOURNAL OF CONTROLLED RELEASE",
+        "journalNumber": 1,
+        "journalVolume": 331,
+        "title": "Intranasal delivery of interferon-beta-loaded nanoparticles induces control of neuroinflammation in a preclinical model of multiple sclerosis: A promising simple, effective, non-invasive, and low-cost therapy",
+        "initialPage": 443,
+        "endPage": 459
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000766602800001",
+        "codeDoi": "10.4067\/s0717-97072021000405316",
+        "typePaper": "Artículo",
+        "journalName": "JOURNAL OF THE CHILEAN CHEMICAL SOCIETY",
+        "journalNumber": 4,
+        "journalVolume": 66,
+        "title": "BRIEF STUDY ON THE DECOMPOSITION OF TETRAETHYLENE GLYCOL DIMETHYL ETHER (TEGDME) SOLVENT IN THE PRESENCE OF Li2O2 AND H2O2",
+        "initialPage": 5316,
+        "endPage": 5319
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000636121800001",
+        "codeDoi": "10.1080\/15548627.2020.1797280",
+        "typePaper": "Revisión",
+        "journalName": "AUTOPHAGY",
+        "journalNumber": 1,
+        "journalVolume": 17,
+        "title": "Guidelines for the use and interpretation of assays for monitoring autophagy (4th edition)",
+        "initialPage": 1,
+        "endPage": 382
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000670141700002",
+        "codeDoi": "10.1016\/j.ejbt.2021.04.004",
+        "typePaper": "Artículo",
+        "journalName": "ELECTRONIC JOURNAL OF BIOTECHNOLOGY",
+        "journalNumber": 1,
+        "journalVolume": 52,
+        "title": "Effects of elderflower extract enriched with polyphenols on antioxidant defense of salmon leukocytes",
+        "initialPage": 13,
+        "endPage": 20
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000603166200001",
+        "codeDoi": "10.1111\/mmi.14667",
+        "typePaper": "Artículo",
+        "journalName": "MOLECULAR MICROBIOLOGY",
+        "journalNumber": 6,
+        "journalVolume": 115,
+        "title": "Genome-wide analysis of in vivo CcpA binding with and without its key co-factor HPr in the major human pathogen group A Streptococcus",
+        "initialPage": 1207,
+        "endPage": 1228
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000629004400001",
+        "codeDoi": "10.2147\/IJN.S260375",
+        "typePaper": "Revisión",
+        "journalName": "INTERNATIONAL JOURNAL OF NANOMEDICINE",
+        "journalNumber": 1,
+        "journalVolume": 16,
+        "title": "The Influence of Size and Chemical Composition of Silver and Gold Nanoparticles on in vivo Toxicity with Potential Applications to Central Nervous System Diseases",
+        "initialPage": 2187,
+        "endPage": 2201
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000592138800001",
+        "codeDoi": "10.1007\/s00217-020-03643-4",
+        "typePaper": "Artículo",
+        "journalName": "EUROPEAN FOOD RESEARCH AND TECHNOLOGY",
+        "journalNumber": 2,
+        "journalVolume": 247,
+        "title": "Acylglycerol synthesis including EPA and DHA from rainbow trout (Oncorhynchus mykiss) belly flap oil and caprylic acid catalyzed by Thermomyces lanuginosus lipase under supercritical carbon dioxide",
+        "initialPage": 499,
+        "endPage": 511
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000716322100016",
+        "codeDoi": "10.1016\/j.bpj.2021.09.026",
+        "typePaper": "Artículo",
+        "journalName": "BIOPHYSICAL JOURNAL",
+        "journalNumber": 21,
+        "journalVolume": 120,
+        "title": "Single-molecule optical tweezers reveals folding steps of the domain swapping mechanism of a protein",
+        "initialPage": 4809,
+        "endPage": 4818
+    },
+    {
+        "year": 2021,
+        "codeWos": "WOS:000549654300001",
+        "codeDoi": "10.1080\/15548627.2020.1782035",
+        "typePaper": "Artículo",
+        "journalName": "AUTOPHAGY",
+        "journalNumber": 7,
+        "journalVolume": 17,
+        "title": "PKD2\/polycystin-2 induces autophagy by forming a complex with BECN1",
+        "initialPage": 1714,
+        "endPage": 1728
+    },
 ]
 
-let request: supertest.SuperAgentTest
-request = supertest.agent(app)
+const request: supertest.SuperAgentTest = supertest.agent(app)
+
+after(async function () {
+    it('Admin user logout', async function () {
+        const res = await request
+            .post(`/auth/sign-off`)
+            .auth(accessToken, { type: 'bearer' })
+            .send({ email: userAdmin.email })
+        expect(res.status).to.equal(204)
+    })
+
+    it('Admin user is removed', async function () {
+        const res = await request
+            .delete(`/users/${adminUserId}`)
+            .auth(accessToken, { type: 'bearer' })
+            .send()
+        expect(res.status).to.equal(204)
+    })
+})
 
 describe('PAPERS endpoints for admin role', function () {
 
-    it('should allow a POST to /users without being logged in', async function () {
+    it('should not allow a GET to /papers without being logged in', async function () {
+        const res = await request
+            .get('/papers')
+            .send()
+        expect(res.status).to.equal(401)
+    })
+
+    it('A user with admin role is created', async function () {
         const res = await request
             .post('/users')
             .send(userAdmin)
@@ -71,21 +209,14 @@ describe('PAPERS endpoints for admin role', function () {
         verificationCode = res.body.verificationCode
     })
 
-    it('should allow a POST to /users/verify/:id/:verificationCode', async function () {
+    it('User admin is verified', async function () {
         const res = await request
             .post(`/users/verify/${adminUserId}/${verificationCode}`)
             .send({})
         expect(res.status).to.equal(200)
     })
 
-    it('should not allow a GET to /papers without being logged in', async function () {
-        const res = await request
-            .get('/papers')
-            .send()
-        expect(res.status).to.equal(401)
-    })
-
-    it('should allow a POST to /auth', async function () {
+    it('Login with the user admin', async function () {
         const res = await request.post('/auth').send(userAdmin)
         expect(res.status).to.equal(200)
         expect(res.body).not.to.be.empty
@@ -93,6 +224,18 @@ describe('PAPERS endpoints for admin role', function () {
         expect(res.body.accessToken).to.be.a('string')
         expect(res.body.refreshToken).to.be.a('string')
         accessToken = res.body.accessToken
+    })
+
+    it('Active year is created', async function () {
+        const res = await request
+            .post(`/years/createNewYear`)
+            .auth(accessToken, { type: 'bearer' })
+            .send()
+        expect(res.status).to.equal(200)
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.be.an('Object')
+        expect(res.body.year).to.be.a('number')
+        activeYear = Number(res.body.year)
     })
 
     describe('With a valid access token', function () {
@@ -128,14 +271,14 @@ describe('PAPERS endpoints for admin role', function () {
                 .put(`/papers/${firstPaperIdTest}`)
                 .auth(accessToken, { type: 'bearer' })
                 .send({
-                    year: 2016,
-                    codeWos: "asdasdasd",
-                    codeDoi: "asdasdasdasd",
+                    year: 2021,
+                    codeWos: "WOS:000568731400017",
+                    codeDoi: "10.1016\/j.jfoodeng.2020.110230",
                     typePaper: "Artículo",
-                    journalName: "Nombre",
+                    journalName: "JOURNAL OF FOOD ENGINEERING",
                     journalNumber: 11,
-                    journalVolume: 1,
-                    title: "Título",
+                    journalVolume: 290,
+                    title: "Manufacture of beta-chitin nano- and microparticles from jumbo squid pen (Dosidicus gigas) and evaluation of their effect on mechanical properties and water vapour permeability of polyvinyl alcohol\/chitosan films",
                     chapterPage: 2,
                     numberOfPages: 22,
                     initialPage: 10,
@@ -171,25 +314,61 @@ describe('PAPERS endpoints for admin role', function () {
                 .post('/papers/massive/import')
                 .auth(accessToken, { type: 'bearer' })
                 .send(importPapers)
-
             expect(res.status).to.equal(204)
         })
 
-        it('should allow a SIGN OFF from /auth/sign-off', async function () {
+        it('Saved Impact Factor and Quartiles for the journals from Journal Object', async () => {
             const res = await request
-                .post(`/auth/sign-off`)
-                .auth(accessToken, { type: 'bearer' })
-                .send({ email: userAdmin.email })
-            expect(res.status).to.equal(204)
-        })
-
-        it('should allow a DELETE from /users/:userId', async function () {
-            const res = await request
-                .delete(`/users/${adminUserId}`)
+                .get(`/journals`)
                 .auth(accessToken, { type: 'bearer' })
                 .send()
-            expect(res.status).to.equal(204)
+            expect(res.status).to.equal(200)
+            expect(res.body).not.to.be.empty
+            expect(res.body).to.be.an('Array')
+            journals = res.body
+
+            for (const journal of journals) {
+                let impactFactor = Array()
+                let quartile = Array()
+                const resSearch = await request
+                    .get(`/journals/search/${journal.name}`)
+                    .auth(accessToken, { type: 'bearer' })
+                    .send()
+                expect(resSearch.status).to.equal(200)
+                expect(resSearch.body).not.to.be.empty
+                expect(resSearch.body).to.be.an('Object')
+                expect(resSearch.body.wosId).to.be.a('string')
+
+                wosId = resSearch.body.wosId
+
+                const resBring = await request
+                    .get(`/journals/bring/${wosId}/year/${activeYear}`)
+                    .auth(accessToken, { type: 'bearer' })
+                    .send()
+                expect(resBring.status).to.equal(200)
+                expect(resBring.body).not.to.be.empty
+                expect(resBring.body).to.be.an('Object')
+
+                impactFactor.push({ year: activeYear, value: Number(resBring.body.journal.jif) })
+                quartile = resBring.body.journal.categories
+
+                const newJournal = {
+                    wosId: wosId,
+                    name: journal.name,
+                    impactFactor: impactFactor,
+                    quartile: quartile
+                }
+
+                const resUpdate = await request
+                    .put(`/journals/name/${journal.name}`)
+                    .auth(accessToken, { type: 'bearer' })
+                    .send(newJournal)
+                expect(resUpdate.status).to.equal(204)
+
+            }
         })
+
+
 
     })
 
