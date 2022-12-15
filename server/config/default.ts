dfa5af4811c3d5a64544f46bee697559aeeefff4
David Marambio <david.marambios@gmail.com>
2022-12-14 23:33:01 -0300

    Setting environment variable for query token to clarivate api

--- server/config/default.ts
+++ server/config/default.ts
@@ -32,6 +32,7 @@ export default {
     logLevel: "info",
     accessTokenTtl: "15m",
     refreshTokenTtl: "1y",
+    clarivateToken: process.env.CLARIVATE_TOKEN,
     smtp: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
